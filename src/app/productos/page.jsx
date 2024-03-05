9425fe3510ccad99204e9267327a3beac0569d35
Salonia04 <agusalonia04@gmail.com>
2024-03-05 17:43:13 -0300

    productos

--- src/app/productos/page.jsx
+++ src/app/productos/page.jsx
@@ -8,17 +8,89 @@ import './productos.css'
 // SIDEBAR
 import Sidebarrr from '@/components/sidebar/Sidebarrr'
 
+// MUI
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import { styled, alpha } from '@mui/material/styles';
+
 // FONT AWESOME
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faPenToSquare, faTrash, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faPenToSquare, faTrash, faXmark, faFileImport } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
-library.add(faPlus, faPenToSquare, faTrash, faXmark);
+library.add(faPlus, faPenToSquare, faTrash, faXmark, faFileImport);
 
 function page() {
+
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+  
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    width: '100%',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
+
   return (
     <>
         <Sidebarrr />
         <div className="container">
+
+          <div className="barra">
+            <div className="contenido-barra">
+              <div className="buscador">
+                <h1>Productos</h1>
+                <Search className='buscar'>
+                  <SearchIconWrapper>
+                    <SearchIcon />
+                  </SearchIconWrapper>
+                  <StyledInputBase
+                    placeholder="Search…"
+                    inputProps={{ 'aria-label': 'search' }}
+                  />
+                </Search>
+              </div>
+              <div className="botones-arriba">
+                <button className='nuevo'><FontAwesomeIcon icon="fa-solid fa-plus" style={{color: "#ffffff",}} size='2xl' /></button>
+                <button className='importar'><FontAwesomeIcon icon="fa-solid fa-file-import" style={{color: "#ffffff",}} size='2xl' /></button>
+              </div>
+            </div>
+          </div>
+
           <div className="productos">
             <div className="producto">
               <div className="degrade"></div>
@@ -37,29 +109,6 @@ function page() {
                 <button className='editar'><FontAwesomeIcon icon="fa-solid fa-pen-to-square" style={{color: "#ffffff",}} size='2xl' /></button>
                 <button className='eliminar'><FontAwesomeIcon icon="fa-solid fa-trash" style={{color: "#ffffff",}} size='2xl' /></button>
               </div>
-            </div>
-          </div>
-
-          <div className="productos2">
-            <div className="producto2">
-              <div className="degrade"></div>
-              <div className="imagen">
-                <img src="https://http2.mlstatic.com/D_NQ_NP_628521-MLA29498939597_022019-O.webp" alt="Img producto" />
-              </div>
-              <hr />
-              <div className="texto">
-                <h3 className='nombre'>Caramelos de Coca Cola</h3>
-              </div>
-              <div className="botones">
-                <button className='agregar'><FontAwesomeIcon icon="fa-solid fa-plus" style={{color: "#ffffff",}} size='2xl' /></button>
-                <button className='editar'><FontAwesomeIcon icon="fa-solid fa-pen-to-square" style={{color: "#ffffff",}} size='2xl' /></button>
-                <button className='eliminar'><FontAwesomeIcon icon="fa-solid fa-trash" style={{color: "#ffffff",}} size='2xl' /></button>
-              </div>
-            </div>
-            <div className="new-bar">
-              <h3 className='codigo'>123456789</h3>
-              <h3 className='cantidad'>x 10</h3>
-              <h3 className='precio'>$ 100</h3>
             </div>
           </div>
         </div>
--- src/components/sidebar/Sidebarrr.jsx
+++ src/components/sidebar/Sidebarrr.jsx
@@ -38,50 +38,48 @@ function Sidebarrr() {
 
   return (
 
-    <div className="general">
-      <div className='all'>
-        <Sidebar
-          width='225px'
-          collapsed={collapsed}
-          backgroundColor='#F56F16'
-          style={{ height: '100vh' }}
-        >
-
-          <div className="stockat">
-            <img className='logo' src="https://media.discordapp.net/attachments/1202708685084823554/1213108492345286686/logoBlanco.png?ex=65f44653&is=65e1d153&hm=08c09a7132f1e489632e60e98757e645c525af6f9b82c1ada91d2b5097d9a21a&=&format=webp&quality=lossless" alt="Logo" />
-            <h1 className={`nombre ${collapsed ? 'hidden' : ''}`}>TOCKAT</h1>
-          </div>
-          <div className="menues">
-            <Menu color='white'>
-              <hr />
-              <a href="/main"><MenuItem style={{ fontSize: '1.3rem', color: 'white' }} icon={<FontAwesomeIcon icon="house" style={{ color: "#ffffff" }} size="xl" />}> Inicio </MenuItem></a>
-              <MenuItem style={{ fontSize: '1.3rem', color: 'white' }} icon={<FontAwesomeIcon icon="cart-shopping" style={{ color: "#ffffff" }} size="xl" />}> Ventas </MenuItem>
-              <a href="/productos">
-                <MenuItem style={{ fontSize: '1.3rem', color: 'white' }} suffix='⚠️' icon={<FontAwesomeIcon icon="box" style={{ color: "#ffffff" }} size="xl" />}> Productos </MenuItem>
-              </a>
-              <MenuItem style={{ fontSize: '1.3rem', color: 'white' }} icon={<FontAwesomeIcon icon="arrow-right-arrow-left" style={{ color: "#ffffff" }} size="xl" />}> Historial </MenuItem>
-            </Menu>
-            <Menu color='white'>
-              <hr />
-              <MenuItem style={{ fontSize: '1.3rem', color: 'white' }} icon={<FontAwesomeIcon icon="download" style={{ color: "#ffffff" }} size="xl" />}> Descargar App </MenuItem>
-            </Menu>
-          </div>
-        </Sidebar>
-        <div className='navbar' style={{ padding: 10 }}>
-          <div className='contenido'>
-            <div className="izquierda">
-              <button style={{ fontSize: '1.3rem', color: 'white' }} className={`sb-button ${collapsed ? 'collapsed' : ''}`} onClick={() => setCollapsed(!collapsed)}>
-                <FontAwesomeIcon icon="fa-solid fa-chevron-up" size="xl" style={{color: "#000000",}} />
-              </button>
-            </div>
-            <div className="derecha">
-              <h1>EL KIOSCO DE MARIO</h1>
-              <hr className='derecha-hr' />
-              <a href="/login"><FontAwesomeIcon icon="fa-solid fa-right-from-bracket" style={{color: "#000000",}} size='xl' /></a>
-            </div>
-          </div>
-          <hr className='hr-final'/>
+    <div className='all'>
+      <Sidebar
+        width='225px'
+        collapsed={collapsed}
+        backgroundColor='#F56F16'
+        style={{ height: '100vh' }}
+      >
+        <div className="stockat">
+          <img className='logo' src="https://media.discordapp.net/attachments/1202708685084823554/1213108492345286686/logoBlanco.png?ex=65f44653&is=65e1d153&hm=08c09a7132f1e489632e60e98757e645c525af6f9b82c1ada91d2b5097d9a21a&=&format=webp&quality=lossless" alt="Logo" />
+          <h1 className={`nombre ${collapsed ? 'hidden' : ''}`}>TOCKAT</h1>
         </div>
+        <div className="menues">
+          <Menu color='white'>
+            <hr />
+            <a href="/main"><MenuItem style={{ fontSize: '1.3rem', color: 'white' }} icon={<FontAwesomeIcon icon="house" style={{ color: "#ffffff" }} size="xl" />}> Inicio </MenuItem></a>
+            <MenuItem style={{ fontSize: '1.3rem', color: 'white' }} icon={<FontAwesomeIcon icon="cart-shopping" style={{ color: "#ffffff" }} size="xl" />}> Ventas </MenuItem>
+            <a href="/productos">
+              <MenuItem style={{ fontSize: '1.3rem', color: 'white' }} suffix='⚠️' icon={<FontAwesomeIcon icon="box" style={{ color: "#ffffff" }} size="xl" />}> Productos </MenuItem>
+            </a>
+            <MenuItem style={{ fontSize: '1.3rem', color: 'white' }} icon={<FontAwesomeIcon icon="arrow-right-arrow-left" style={{ color: "#ffffff" }} size="xl" />}> Historial </MenuItem>
+          </Menu>
+          <Menu color='white'>
+            <hr />
+            <MenuItem style={{ fontSize: '1.3rem', color: 'white' }} icon={<FontAwesomeIcon icon="download" style={{ color: "#ffffff" }} size="xl" />}> Descargar App </MenuItem>
+          </Menu>
+        </div>
+      </Sidebar>
+
+      <div className='navbar' style={{ padding: 10 }}>
+        <div className='contenido'>
+          <div className="izquierda">
+            <button style={{ fontSize: '1.3rem', color: 'white' }} className={`sb-button ${collapsed ? 'collapsed' : ''}`} onClick={() => setCollapsed(!collapsed)}>
+              <FontAwesomeIcon icon="fa-solid fa-chevron-up" size="xl" style={{color: "#000000",}} />
+            </button>
+          </div>
+          <div className="derecha">
+            <h1>EL KIOSCO DE MARIO</h1>
+            <hr className='derecha-hr' />
+            <a href="/login"><FontAwesomeIcon icon="fa-solid fa-right-from-bracket" style={{color: "#000000",}} size='xl' /></a>
+          </div>
+        </div>
+        <hr className='hr-final'/>
       </div>
     </div>
   )
